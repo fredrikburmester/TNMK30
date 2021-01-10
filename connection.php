3b8f38fe3865694df6809d5ab1356d07f6f237d6
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-01-10 20:22:28 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,14 +1,9 @@
 <?php
 
-	/*$serverIP = "10.209.41.4";
-	$dbusername = "2011051_bx94712";
-	$dbpassword = "hacker97";
-	$dbname = "2011051-dummy";*/
-
-	$serverIP = "10.209.41.4";
-	$dbusername = "2011051_db76699";
-	$dbpassword = "hacker97";
-	$dbname = "2011051-lego-2016";
+	$serverIP = "";
+	$dbusername = "";
+	$dbpassword = "";
+	$dbname = "";
 
 	$conn = mysqli_connect($serverIP, $dbusername,$dbpassword,$dbname);
  	if(mysqli_errno($conn)) {
